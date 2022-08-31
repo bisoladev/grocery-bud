edb14f2009b1f63acf65c3885348388d87a8bfcf
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-08-31 20:12:24 +0100

    removeItem function created

--- src/App.js
+++ src/App.js
@@ -42,6 +42,11 @@ function App() {
     showAlert(true, "danger", "empty list");
     setList([]);
   };
+
+  const removeItem = (id) => {
+    showAlert(true, "danger", "item removed");
+    setList(list.filter((item) => item.id !== id));
+  };
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
@@ -64,7 +69,7 @@ function App() {
       </form>
       {list.length > 0 && (
         <div className="grocery-container">
-          <List items={list} />
+          <List items={list} removeItem={removeItem} />
           <button className="clear-btn" onClick={clearList}>
             Clear Items
           </button>
