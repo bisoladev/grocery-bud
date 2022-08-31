8ecf0b011c2cfbafe2178695c56c466810c6f8fe
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-08-31 19:24:24 +0100

    Decreased timeout to 2seconds

--- src/Alert.js
+++ src/Alert.js
@@ -2,15 +2,15 @@ import React, { useEffect } from "react";
 
 const Alert = ({ type, msg, removeAlert }) => {
   useEffect(() => {
-    const timeout = setTimeout(()=> {
-      removeAlert()
-    },3000)
-  
+    const timeout = setTimeout(() => {
+      removeAlert();
+    }, 2000);
+
     return () => {
-      clearTimeout(timeout)
-    }
-  }, [])
-  
+      clearTimeout(timeout);
+    };
+  }, []);
+
   return <p className={`alert alert-${type}`}>{msg}</p>;
 };
 
